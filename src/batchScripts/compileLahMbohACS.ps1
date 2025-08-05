787238a9c1a2c5e280babfc6c00be06bf38a4818
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2025-08-05 11:00:25 +0700

    atypo

---
+++ src/batchScripts/compileLahMbohACS.ps1
@@ -0,0 +1,28 @@
+# https://stackoverflow.com/a/18848848/9079640
+# https://stackoverflow.com/a/2591782/9079640
+# https://www.atera.com/blog/how-to-run-exe-files-in-powershell/
+# pls why tf ACC does not itterate?!??!??!?!
+# (JOELwindows7)
+# hey Windsurf, if you screw up, idk..
+# Ugh, this is embarrasing!! using assistive coding bot / AI Coding?! 
+# in this age where people thought steam machine took over their job but now here we got EV train??
+# My aura is gone!!!!
+# This is heavy dillema!!
+# I didn't prompt anything! I come the code from my mind!
+# I know what I am doing!
+
+$FolderSource = ".\source"
+$FolderCompile = ".\acs"
+$Compiler = ".\yoinks\ZDoom\acs\acc-win32.exe"
+
+Write-Output "Let's compile ACS with $($Compiler)"
+
+Get-ChildItem $FolderSource -Filter *.acs | ForEach-Object {
+    $ObtainedSource = "$($FolderSource)\$($_.Name)"
+    $ObtainedTarget = "$($FolderCompile)\$($_.BaseName).o"
+    # Write-Output "Compiling $($_.FullName) to $($FolderCompile)\$($_.BaseName).o"
+    Write-Output "Compiling $($ObtainedSource) to $($ObtainedTarget)"
+    #.\yoinks\ZDoom\acs\acc-win32.exe $($_.Name)
+    Invoke-Expression "$($Compiler) $($ObtainedSource) $($ObtainedTarget)"
+    
+}
---
+++ src/batchScripts/compileLahMbohACS.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Same compile but for Linux
+# (JOELwindows7)
+# https://stackoverflow.com/a/1521498/9079640
+# https://unix.stackexchange.com/a/711377/390514
+# https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+# https://superuser.com/a/731250/1036816
+# https://askubuntu.com/a/804082 must also aware of architecture!
+
+
+folderSource="./source"
+folderCompile="./acs"
+compiler="./yoinks/ZDoom/acs/acc-amd64"
+
+echo "Firstly, check the CPU Architecture!:"
+
+# direct yoink, edit now! Change app based on this!
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    echo "X64 Architecture"
+    compiler="./yoinks/ZDoom/acs/acc-amd64"
+elif [[ $arch == i*86 ]]; then
+    echo "X32 Architecture"
+    compiler="./yoinks/ZDoom/acs/acc-i386"
+elif  [[ $arch == arm* ]]; then
+    echo "ARM Architecture"
+    compiler="./yoinks/ZDoom/acs/acc-arm64"
+elif  [[ $arch == riscv ]]; then
+    echo "RISC-V Architecture"
+    compiler="./yoinks/ZDoom/acs/acc-riscv64" # oh shit! We don't have RISC-V version!!!!!!!!
+fi
+
+echo "Time to Compile with $compiler"
+
+for anFile in ./source/*.acs; do
+    baseFilename=$(basename -- "$anFile")
+    extensionFile="${baseFilename##*.}"
+    justFilename=${baseFilename%.*}
+    obtainedSource="$anFile"
+    obtainedTarget="$folderCompile/$justFilename.o"
+    echo "Compile $obtainedSource to $obtainedTarget"
+    $compiler "$obtainedSource" "$obtainedTarget"
+done
